9b636e328c843646f40bdd72ee5f8ed67dd719dd
Súsanna Trusty <susannaelinn@gmail.com>
2026-05-18 16:09:36 +0000

    Update location.rs

--- location.rs
+++ location.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::convert::TryFrom;
 
 pub struct Location {
     pub building: String,
@@ -17,5 +18,41 @@ impl fmt::Display for Location {
             self.floor,
             self.room
         )
+    }
+}
+
+impl TryFrom<&str> for Location {
+
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+
+        let parts: Vec<&str> = value.split('-').collect();
+
+        if parts.len() != 2 {
+            return Err(String::from("Invalid format"));
+        }
+
+        let building = parts[0].to_string();
+
+        let numbers = parts[1];
+
+        if numbers.len() < 3 {
+            return Err(String::from("Invalid room format"));
+        }
+
+        let floor = numbers[0..1]
+            .parse::<u8>()
+            .map_err(|_| String::from("Invalid floor"))?;
+
+        let room = numbers[1..]
+            .parse::<u16>()
+            .map_err(|_| String::from("Invalid room"))?;
+
+        Ok(Location {
+            building,
+            floor,
+            room,
+        })
     }
 }
