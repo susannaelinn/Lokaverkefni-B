64e28499c443f15e6b0fc4b2150d8aa52cc00678
Súsanna Trusty <susannaelinn@gmail.com>
2026-05-18 15:29:51 +0000

    Update location.rs

--- location.rs
+++ location.rs
@@ -1,5 +1,21 @@
+use std::fmt;
+
 pub struct Location {
     pub building: String,
     pub floor: u8,
     pub room: u16,
+}
+
+impl fmt::Display for Location {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+        write!(
+            f,
+            "{}-{}{}",
+            self.building,
+            self.floor,
+            self.room
+        )
+    }
 }
