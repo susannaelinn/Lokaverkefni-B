5c6ca63c710fd7aa854bc981475822178f7d577b
Súsanna Trusty <susannaelinn@gmail.com>
2026-05-18 16:08:05 +0000

    Update main.rs

--- main.rs
+++ main.rs
@@ -4,30 +4,58 @@ mod chair;
 mod projector;
 mod location;
 mod category;
+mod database;
+
+use std::convert::TryFrom;
 
 use equipment::Equipment;
 use location::Location;
 use category::EquipmentCategory;
 use desk::Desk;
 
-fn main() {
+use database::create_database;
+use database::insert_equipment;
+use database::list_equipment;
+use database::delete_equipment;
 
-    let location = Location {
-        building: String::from("HA"),
-        floor: 2,
-        room: 301,
-    };
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let conn = create_database()?;
+
+    println!("Database connected.");
+
+    let location = Location::try_from("HA-2301")?;
 
     let desk = Desk {
         seats: 4,
     };
 
     let equipment = Equipment {
-        id: 1,
+
         value: 50000,
+
         location,
+
         category: EquipmentCategory::Desk(desk),
     };
 
-    println!("Location: {}", equipment.location);
+    insert_equipment(&conn, &equipment)?;
+
+    println!("Equipment inserted.");
+
+    println!("\nEquipment List:");
+
+    list_equipment(&conn)?;
+
+    println!("\nDeleting equipment with ID 1...");
+
+    delete_equipment(&conn, 1)?;
+
+    println!("Deleted.");
+
+    println!("\nUpdated Equipment List:");
+
+    list_equipment(&conn)?;
+
+    Ok(())
 }
